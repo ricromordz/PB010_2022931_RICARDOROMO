fbc946c2faa4265acaffe16654f80d00e75c9948
ricromordz <100107290+ricromordz@users.noreply.github.com>
2022-02-24 15:19:20 -0600

    char a string y uso de cin.ignore y getline
    string funciona igual que char en el sentido que ambos considera un espacio como salto de linea pero string permite usar comandos para evitar eso
    al tener problema con que el programa no permitia saltar espacios entre palabras cambie las partes de char por cin.ignore (que en este caso complementa al getline evitando que salte procesos) y getline (salta de linea colocando el codigo posterior junto al actual)
    tambien quite el system(cls) que estaba en agregar cita para que se pudiese ver las citas agregadas anteriormente si no se salio al menu

--- Avance 1.cpp
+++ Avance 1.cpp
@@ -7,13 +7,14 @@ int main() {
 
 	bool menu = true, paciente = true;
 	float preciotrat = 0, precio = 0, preciototal = 0;
-	char nombre[40] = "", tratamiento[40] = "", hora[10] = "";
+	//char nombre[40] = "", tratamiento[100] = "", hora[10] = "", Ntratamiento[100] = "";
+	string nombre, tratamiento, hora, Ntratamiento;  // string funciona igual que char en el sentido que ambos considera un espacio como salto de linea pero string permite usar comandos para evitar eso 
 	int opcion = 0, CT = 0, num = 0;
 	cout << "\t\t\t\t\tBienvenido a su clinica dentista" << endl;
 	cout << "Para abrir el menu de opciones ingrese un numero" << endl;
 	cin >> num;
-	while (menu) {
-
+	while(menu){
+		
 		cout << "Ingrese 1 para Agendar una cita\n \t2 para Modificar una cita\n \t3 para Eliminar una cita\n \t4 para revisar la lista de citas vigentes" << endl;
 		cout << "\t5 para limpiar la pantalla\n \t0 para salir" << endl;
 		cin >> opcion;
@@ -28,13 +29,18 @@ int main() {
 
 			while (paciente) {
 				cout << "Nombre de paciente:";
-				cin >> nombre;
+				cin.ignore(); //sirve para ignorar el salto de linea de getline
+				getline(cin, nombre); // salta de linea colocando el codigo posterior junto al actual
+				//cin >> nombre;
 				cout << "Hora del tratamiento:";
-				cin >> hora;
+				getline(cin, hora);
+				//cin >> hora;
 				cout << "Nombre del tratamiento:";
-				cin >> tratamiento;
+				getline(cin, Ntratamiento);
+				//cin >> Ntratamiento;
 				cout << "Descripcion del tratamiento:";
-				cin >> tratamiento;
+				getline(cin, tratamiento);
+				//cin >> tratamiento;
 				cout << "Precio unitario del tratamiento:";
 				cin >> preciotrat;
 				cout << "Cantidad del tratamiento:";
@@ -48,25 +54,31 @@ int main() {
 				cin >> num;
 				if (num == 6)
 					paciente = false;
-				system("cls");
 			}
 			paciente = true;
 			break;
 		case 2:
 			//Modifica los valores anteriores
 			cout << "Nombre de paciente: " << nombre << endl;
+			cout << "Ingrese el nuevo valor:";
 			cin >> nombre;
 			cout << "Hora del tratamiento:" << hora << endl;
+			cout << "Ingrese el nuevo valor:";
 			cin >> hora;
-			cout << "Nombre del tratamiento:" << tratamiento << endl;
-			cin >> tratamiento;
+			cout << "Nombre del tratamiento:" << Ntratamiento << endl;
+			cout << "Ingrese el nuevo valor:";
+			cin >> Ntratamiento;
 			cout << "Descripcion del tratamiento:" << tratamiento << endl;
+			cout << "Ingrese el nuevo valor:";
 			cin >> tratamiento;
 			cout << "Precio unitario del tratamiento:" << preciotrat << endl;
+			cout << "Ingrese el nuevo valor:";
 			cin >> preciotrat;
 			cout << "Cantidad del tratamiento:" << CT << endl;
+			cout << "Ingrese el nuevo valor:";
 			cin >> CT;
 			cout << "Precio unitario:" << precio << endl;
+			cout << "Ingrese el nuevo valor:";
 			cin >> precio;
 			cout << "Total(Pesos mexicanos):" << endl;
 			preciototal = precio + preciotrat;
@@ -76,7 +88,7 @@ int main() {
 			//Muestra los valores actualizados
 			cout << "Nombre de paciente: " << nombre << endl;
 			cout << "Hora del tratamiento:" << hora << endl;
-			cout << "Nombre del tratamiento:" << tratamiento << endl;
+			cout << "Nombre del tratamiento:" << Ntratamiento << endl;
 			cout << "Descripcion del tratamiento:" << tratamiento << endl;
 			cout << "Precio unitario del tratamiento:" << preciotrat << endl;
 			cout << "Cantidad del tratamiento:" << CT << endl;
@@ -89,10 +101,15 @@ int main() {
 			break;
 
 		case 3:
-
-			strcpy_s(nombre, ""); //copia el valor actual de la variable y se usa una coma para luego poner el nuevo valor
+		
+			nombre = "";
+			hora = "";
+			tratamiento = "";
+			Ntratamiento = "";
+			/*strcpy_s(nombre, ""); //copia el valor actual de la variable y se usa una coma para luego poner el nuevo valor
 			strcpy_s(hora, "");
-			strcpy_s(tratamiento, "");
+			strcpy_s(Ntratamiento, "");
+			strcpy_s(tratamiento, "");*/
 			preciotrat = 0;
 			CT = 0;
 			precio = 0;
@@ -106,7 +123,7 @@ int main() {
 			//Muestra los valores actualizados
 			cout << "Nombre de paciente: " << nombre << endl;
 			cout << "Hora del tratamiento:" << hora << endl;
-			cout << "Nombre del tratamiento:" << tratamiento << endl;
+			cout << "Nombre del tratamiento:" << Ntratamiento << endl;
 			cout << "Descripcion del tratamiento:" << tratamiento << endl;
 			cout << "Precio unitario del tratamiento:" << preciotrat << endl;
 			cout << "Cantidad del tratamiento:" << CT << endl;
@@ -121,11 +138,3 @@ int main() {
 		}
 	}
 }
-
-
-
-
-
-
-
-
